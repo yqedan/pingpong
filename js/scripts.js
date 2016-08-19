d22c0c7f50b4522d63ddba68ec80755f887fddb1
Yusuf Qedan <yusuf9191@gmail.com>
2016-08-19 14:06:32 -0700

    add event listener on browser window size to dynamically adjust size of divs on screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,43 @@
+var formDivHeight = 250;
 function pingpong(numberEntered){
   if (numberEntered % 15 == 0)
-    return "ping-pong";
+  return "ping-pong";
   else if(numberEntered % 3 == 0)
-    return "ping";
+  return "ping";
   else if (numberEntered % 5 == 0)
-    return "pong";
+  return "pong";
   else
-    return numberEntered;
+  return numberEntered;
 };
-
 $(document).ready(function() {
+  $(window).resize(function(){
+    if($( window ).width() <= 599){
+      $("#mysidebarBackground").css("height", 250);
+    }
+    else{
+      $("#mysidebarBackground").css("height", formDivHeight);
+    }
+  });
   $("form").submit(function(){
     event.preventDefault();
+    formDivHeight = 250;
+    var numberEntered = parseInt($("#numberEntered").val());
     $(".tempListItem").remove();
     $("#numberList").hide();
-    var numberEntered = parseInt($("#numberEntered").val());
-    if(101 > numberEntered && numberEntered > 0 )
-      for (var i = 1; i <= numberEntered; i++)
+    $("#myFormBackground").css("height", formDivHeight);
+    $("#mysidebarBackground").css("height", formDivHeight);
+    if(101 > numberEntered && numberEntered > 0 ){
+      formDivHeight += (20 * (numberEntered - 1));
+      for (var i = 1; i <= numberEntered; i++){
         $("#numberList").append("<li class='tempListItem'>" + pingpong(i) + "</li>");
-    else
-      $("#numberList").append("<li class='tempListItem'>" + "Please Enter a positive integer up to 100!" + "</li>");
-    $("#numberList").slideDown(200);
+      }
+      $("#myFormBackground").css("height", formDivHeight);
+      $("#mysidebarBackground").css("height", formDivHeight);
+      $("#numberList").slideDown(1000);
+    }
+    else{
+      $("#numberList").slideDown(200);
+      $("#numberList").append("<p class='tempListItem' style='text-align:center'>" + "Please Enter a positive integer up to 100!" + "</p>");
+    }
   });
 });
