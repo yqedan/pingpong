f41c48837a47b869e7758e3b84af694f7793c755
Yusuf Qedan <yusuf9191@gmail.com>
2016-08-19 08:12:45 -0700

    add behavior: list item 1 added if user enters a 1, item removed when for is re submitted input: 1 output: 1 formatted into a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
+function pingpomng(){};
 
+$(document).ready(function() {
+  $("form").submit(function(){
+    event.preventDefault();
+    $("#tempList").remove();
+    var numberEntered = parseInt($("#numberEntered").val());
+    alert(numberEntered);
+    $("#numberList").append("<li id='tempList'>" + numberEntered + "</li>");
+    // for (var i = 0; i < numberEntered; i++) {
+    //
+    // }
+  });
 });
