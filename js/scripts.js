a264bab4e202e5da9963a3e7f4881896c446dae6
Yusuf Qedan <Yusuf Qedan>
2016-08-22 21:58:46 -0700

    rename a variable to appropriate name, seperate error checking from UI logic, make div for sidebar and form taller

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //Global Variables
-var formDivHeight = 250;
+var formDivHeight = 290;
 var submitDivHeightSmall = 150;
 
 //Business Logic
@@ -18,23 +18,32 @@ function pingpong(numberEntered){
   }
 };
 function reverse(word){
-  var reverseword = "";
+  var reverseWord = "";
   var wordArr = word.split("");
   var wordArrReverse = [];
   for (var i = (wordArr.length-1); i >= 0; i--) {
     wordArrReverse.push(wordArr[i]);
   }
-  reverseName = wordArrReverse.join("");
-  return reverseName;
+  reverseWord = wordArrReverse.join("");
+  return reverseWord;
 }
+
 function calcDivHeight(numberEntered){
   formDivHeight += (20 * (numberEntered - 1));
 }
 
+function validateInput(number){
+  if(101 > number && number > 0 ){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 //User interface logic
 $(document).ready(function() {
   $(window).resize(function(){
-    if(formDivHeight == 250){
+    if(formDivHeight == 290){
       if($(window).width() <= 720){
         $("#mysidebarBackground").css("height", submitDivHeightSmall);
       }
@@ -65,7 +74,8 @@ $(document).ready(function() {
     $("#myFormBackground").css("height", formDivHeight);
     $("#mysidebarBackground").css("height", formDivHeight);
     var numberEntered = parseInt($("#numberEntered").val());
-    if(101 > numberEntered && numberEntered > 0 ){
+    var validInput = validateInput(numberEntered);
+    if(validInput){
       calcDivHeight(numberEntered);
       for (var i = 1; i <= numberEntered; i++){
         $("#numberList").append("<li class='tempListItem'>" + pingpong(i) + "</li>");
